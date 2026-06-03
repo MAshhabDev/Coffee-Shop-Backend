87aaebacfb7e4b107de59c50e03a50bdd3f3ceae
Mahir4098 <mahirjr98@gmail.com>
2026-06-03 20:59:50 +0600

    Create All the necessary table for my coffee store app

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,7 +5,8 @@ export const pool = new Pool({
   connectionString: config.connection_string,
 });
 
-const initDB = async () => {
+export const initDB = async () => {
+  // users table
   await pool.query(`
         CREATE TABLE IF NOT EXISTS users(
         id SERIAL PRIMARY KEY,
@@ -15,8 +16,50 @@ const initDB = async () => {
         role VARCHAR(20) DEFAULT 'users',
 
         created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
 
         )
         `);
+
+  // Coffees table
+
+  await pool.query(`
+            CREATE TABLE IF NOT EXISTS coffees(
+            id SERIAL PRIMARY KEY,
+
+            name VARCHAR(20) NOT NULL,
+            category VARCHAR(50) NOT NULL,
+            price int,
+           created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+
+
+            )
+            `);
+
+  // Orders Table
+
+  await pool.query(`
+                CREATE TABLE IF NOT EXISTS orders(
+            id SERIAL PRIMARY KEY,
+            user_id INT REFERENCES users(id) ON DELETE CASCADE,
+
+            total_price int,
+            status VARCHAR(50) DEFAULT 'pending',
+           created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+  )
+                `);
+
+  await pool.query(`
+                CREATE TABLE IF NOT EXISTS order_items(
+            id SERIAL PRIMARY KEY,
+            order_id INT  REFERENCES orders(id) ON DELETE CASCADE,
+            coffee_id INT  REFERENCES coffees(id) ON DELETE CASCADE,
+price int,
+            quantity int,
+           created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+  )
+                `);
 };
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import app from "./app";
 import config from "./config";
+import { initDB } from "./db";
 
-const main = () => {
+const main = async () => {
+  await initDB();
   app.listen(config.port, () => {
     console.log(`Coffee Server App Listening On Port ${config.port}`);
   });
