1efbfd73bb8f2fd3322d702b369fc4056e8ba82c
Mahir4098 <mahirjr98@gmail.com>
2026-06-04 17:41:30 +0600

    Implement The Auth Modules

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import type { Request, Response } from "express";
+
+
+
+const signUp=(req:Request, res:Response)=>{
+try {
+    
+} catch (error) {
+    
+}
+}
+
+
+export  const authController={signUp}
